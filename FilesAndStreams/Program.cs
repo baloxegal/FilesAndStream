62419aef40dcb7224672f560f373fe33cd4d91a1
baloxegal <baloxegal@gmail.com>
2021-03-19 15:01:10 +0200

    Final

--- FilesAndStreams/Program.cs
+++ FilesAndStreams/Program.cs
@@ -9,46 +9,141 @@ namespace FilesAndStreams
     class Program
     {
         static void Main(string[] args)
-        {            
-            string dir_watcher_name = @"D:\Amdaris\WatcherFolder\";
-
+        {
             //This is My FileChecker
 
-            //string dir_1_name = @"D:\Amdaris\first\";
-            //string dir_2_name = @"D:\Amdaris\second\";
+            string dir_1_name = @"D:\Amdaris\first\";
+            string dir_2_name = @"D:\Amdaris\second\";
+            string dir_3_name = @"D:\Amdaris\backup\";
 
-            //Directory.CreateDirectory(dir_1_name);
-            //List<string> last_list_files_1 = Directory.GetFiles(dir_1_name).ToList();
-            //List<string> new_list_files_1 = new List<string>();
+            if (!Directory.Exists(dir_1_name))
+                Directory.CreateDirectory(dir_1_name);
 
-            //while (true)
-            //{
-            //    if (!Directory.Exists(dir_1_name))
-            //        Directory.CreateDirectory(dir_1_name);
+            List<string> last_list_files_1 = Directory.GetFiles(dir_1_name).ToList();
+            List<string> new_list_files_1 = new List<string>();
 
-            //    new_list_files_1 = Directory.GetFiles(dir_1_name).ToList();
+            while (true)
+            {
+                if (!Directory.Exists(dir_3_name))
+                    Directory.CreateDirectory(dir_3_name);
 
-            //    if (last_list_files_1.OrderBy(s => s).SequenceEqual(new_list_files_1.OrderBy(s => s)))
-            //        continue;
+                if (!Directory.Exists(dir_2_name))
+                    Directory.CreateDirectory(dir_2_name);
 
-            //    var except_new = new_list_files_1.Except(last_list_files_1);
-            //    var except_last = last_list_files_1.Except(new_list_files_1);
+                if (!Directory.Exists(dir_1_name))
+                    Directory.CreateDirectory(dir_1_name);
 
-            //    last_list_files_1 = new_list_files_1;
+                new_list_files_1 = Directory.GetFiles(dir_1_name).ToList();
 
-            //    if (!Directory.Exists(dir_2_name))
-            //        Directory.CreateDirectory(dir_2_name);
+                var list_1 = last_list_files_1.OrderBy(s => s);
+                var list_2 = new_list_files_1.OrderBy(s => s);
 
-            //    foreach (var p in except_new)
-            //        File.Copy(p, p.Replace("first", "second"));
+                if (list_1.SequenceEqual(list_2))
+                {
+                    for (int i = 0; i < list_1.Count(); i++)
+                    {
+                        FileInfo f1 = new FileInfo(list_1.ElementAt(i));
+                        FileInfo f2 = new FileInfo(list_1.ElementAt(i).Replace("first", "second"));
+                        
+                        FileStream fs1 = f1.OpenRead();
+                        FileStream fs2 = f2.OpenRead();
+                        if (!fs1.Length.Equals(fs2.Length))
+                        {
+                            if (File.Exists(f1.ToString()) && File.Exists(f2.ToString()))
+                                File.Replace(f1.ToString(), f2.ToString(), f1.ToString().Replace("first", "backup"));
+                            continue;
+                        }
+                        int file1byte;
+                        int file2byte;
+                        do
+                        {
+                            file1byte = fs1.ReadByte();
+                            file2byte = fs2.ReadByte();
+                        }
+                        while ((file1byte == file2byte) && (file1byte != -1));
 
-            //    foreach (var p in except_last)
-            //        File.Delete(p.Replace("second", "first"));
+                        fs1.FlushAsync();
+                        fs2.FlushAsync();
+                        fs1.Close();
+                        fs2.Close();
 
-            //    Thread.Sleep(5000);
-            //}
+
+                        // IS HASH METHOD IN GOOD PRACTICE
+
+
+
+                        //byte[] hash1 = ha.ComputeHash(f1);
+                        //byte[] hash2 = ha.ComputeHash(f2);
+                        //f1.Close();
+                        //f2.Close();
+                        ///* Show Hash in TextBoxes */
+                        //txtHash1.Text = BitConverter.ToString(hash1);
+                        //txtHash2.Text = BitConverter.ToString(hash2);
+                        ///* Compare the hash and Show Message box */
+                        //if (txtHash1.Text == txtHash2.Text)
+                        //{
+                        //    MessageBox.Show("Files are Equal !");
+                        //}
+                        //else
+                        //{
+                        //    MessageBox.Show("Files are Diffrent !");
+                        //}
+                    }
+                    continue;
+                }
+
+                var except_new = new_list_files_1.Except(last_list_files_1);
+                var except_last = last_list_files_1.Except(new_list_files_1);
+                var intersect_files = last_list_files_1.Intersect(new_list_files_1);
+
+                foreach (var p in except_new)
+                    File.Copy(p, p.Replace("first", "second"));
+
+                foreach (var p in except_last)
+                    File.Delete(p.Replace("first", "second"));
+
+                foreach (var p in intersect_files)
+                {
+                    FileInfo f1 = null;
+                    FileInfo f2 = null;
+                    if (File.Exists(p) && File.Exists(p.Replace("first", "second")))
+                    {
+                        f1 = new FileInfo(p);
+                        f2 = new FileInfo(p.Replace("first", "second"));
+                    }
+                    f1.Refresh();
+                    f2.Refresh();
+                    FileStream fs1 = f1.OpenRead();
+                    FileStream fs2 = f2.OpenRead();
+                    if (!fs1.Length.Equals(fs2.Length))
+                    {
+                        if (File.Exists(f1.ToString()) && File.Exists(f2.ToString()))
+                            File.Replace(f1.ToString(), f2.ToString(), f1.ToString().Replace("first", "backup"));
+                        continue;
+                    }
+                    int file1byte;
+                    int file2byte;
+                    do
+                    {
+                        file1byte = fs1.ReadByte();
+                        file2byte = fs2.ReadByte();
+                    }
+                    while ((file1byte == file2byte) && (file1byte != -1));
+
+                    fs1.FlushAsync();
+                    fs2.FlushAsync();
+                    fs1.Close();
+                    fs2.Close();
+                }
+                last_list_files_1 = new_list_files_1;
+
+                Thread.Sleep(5000);
+            }
 
             //This is Microsoft FileChecker
+
+
+            string dir_watcher_name = @"D:\Amdaris\WatcherFolder\";
 
             /*using*/
             var watcher = new FileSystemWatcher(dir_watcher_name);
@@ -62,13 +157,12 @@ namespace FilesAndStreams
                                  | NotifyFilters.Security
                                  | NotifyFilters.Size;
 
-
             //new FileSystemEventHandler(OnChanged)
-            watcher.Changed += OnChanged;
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
             watcher.Created += OnCreated;
             watcher.Deleted += OnDeleted;
-            watcher.Renamed += OnRenamed;
-            
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
             watcher.Filter = "*.txt";
             watcher.IncludeSubdirectories = true;
             watcher.EnableRaisingEvents = true;
@@ -76,7 +170,7 @@ namespace FilesAndStreams
             //This is Driver Info
 
             DriveInfo[] myDrives = DriveInfo.GetDrives();
-             
+
             foreach (DriveInfo d in myDrives)
             {
                 Console.WriteLine("Name: {0}", d.Name);
@@ -108,7 +202,7 @@ namespace FilesAndStreams
                     reader.Close();
             }
 
-            //Writer
+            //FailWriter
 
             string[] myTasks = {"Repair water tap",
                     "Buy bread and milk",
@@ -123,8 +217,8 @@ namespace FilesAndStreams
             }
 
 
-            Console.WriteLine("Press enter to exit.");
-            Console.ReadLine();
+            //Console.WriteLine("Press enter to exit.");
+            //Console.ReadLine();
         }
         private static void OnChanged(object sender, FileSystemEventArgs e)
         {
